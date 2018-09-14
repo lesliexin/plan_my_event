4ace8518e8e12234ee4cc4d796863e669d42d613
Leslie Xin <lesliexin@MacBook-Pro-4.local>
2018-09-14 16:15:27 -0400

    Added .gitignore

--- my_event_site/event_app/admin.py
+++ my_event_site/event_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Event
+
+# allows us to see Question model in admin interface
+admin.site.register(Event)
--- my_event_site/event_app/models.py
+++ my_event_site/event_app/models.py
@@ -1,3 +1,23 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Event( models.Model ):
+	name = models.CharField(max_length = 200)
+	date = models.DateTimeField('date published')
+
+	def __str__(self):
+		return self.name
+
+class Person( models.Model ):
+	event = models.ForeignKey(Event, on_delete = models.CASCADE)
+	first_name = models.CharField(max_length = 50)
+	last_name = models.CharField(max_length = 50)
+	attendance_status = attendance_status = models.CharField(max_length = 10)
+
+	def __str__(self):
+		return self.first_name
+
+
