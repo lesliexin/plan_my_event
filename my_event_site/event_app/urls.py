753b36043d264f6c20391e9a772d0a50c7665801
Leslie Xin <lesliexin@MacBook-Pro-4.local>
2018-09-17 10:16:59 -0400

    Created basic view and html page

---
+++ my_event_site/event_app/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.event_list, name='event_list'),
+]
--- my_event_site/event_app/views.py
+++ my_event_site/event_app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+
+from .models import Event, Person
 
 # Create your views here.
+def event_list(request):
+    return render(request, 'event_app/event_list.html', {})
