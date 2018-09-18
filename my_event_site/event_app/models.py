8d5a5ccf7aed9c657e2c60f4274746e04c765298
Leslie Xin <lesliexin@MacBook-Pro-4.local>
2018-09-18 14:04:54 -0400

    added new models for lists and items, displayed them on the event_details page

--- my_event_site/event_app/models.py
+++ my_event_site/event_app/models.py
@@ -3,21 +3,53 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+PRIORITY_CHOICES = ( 
+  (1, 'Low'), 
+  (2, 'Normal'), 
+  (3, 'High'), 
+) 
+
 # Create your models here.
 class Event( models.Model ):
 	name = models.CharField(max_length = 200)
 	date = models.DateTimeField('date published')
 
+
 	def __str__(self):
 		return self.name
 
+class List(models.Model): 
+  title = models.CharField(max_length=250, default="", unique=True) 
+  an_event = models.ForeignKey(Event, on_delete = models.CASCADE) 
+  def __str__(self): 
+    return self.title 
+  class Meta: 
+    ordering = ['title'] 
+  class Admin: 
+    pass
+
 class Person( models.Model ):
-	event = models.ForeignKey(Event, on_delete = models.CASCADE)
-	first_name = models.CharField(max_length = 50)
-	last_name = models.CharField(max_length = 50)
-	attendance_status = attendance_status = models.CharField(max_length = 10)
+	a_list = models.ForeignKey(List, on_delete = models.CASCADE)
+	full_name = models.CharField(max_length=50, default="") 
+	created_date = models.DateTimeField(default=datetime.datetime.now) 
+	will_attend = models.BooleanField(default=False)
 
 	def __str__(self):
-		return self.first_name
+		return self.full_name
+	class Meta:
+		ordering = ['full_name'] 
+	class Admin: 
+		pass
 
-
+class Item(models.Model): 
+  title = models.CharField(max_length=250, default="") 
+  created_date = models.DateTimeField(default=datetime.datetime.now) 
+  priority = models.IntegerField(choices=PRIORITY_CHOICES, default=2) 
+  completed = models.BooleanField(default=False) 
+  todo_list = models.ForeignKey(List, on_delete = models.CASCADE) 
+  def __str__(self): 
+    return self.title 
+  class Meta: 
+    ordering = ['-priority', 'title'] 
+  class Admin: 
+    pass
--- my_event_site/event_app/views.py
+++ my_event_site/event_app/views.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 
-from .models import Event, Person
+from .models import Event, Person, List
 from .forms import EventForm
 
 # Create your views here.
@@ -14,7 +14,10 @@ def event_list(request):
 
 def event_detail(request, pk):
     event = get_object_or_404(Event, pk=pk)
-    return render(request, 'event_app/event_detail.html', {'event': event})
+    list_obj = get_object_or_404(List, pk=pk)
+    lists1 = Event.objects.get(id=pk)
+    lists = lists1.list_set.all()
+    return render(request, 'event_app/event_detail.html', {'lists': lists})
 
 def event_new(request):
 	if request.method == "POST":
